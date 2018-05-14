70b6c054808e7c12c9d9ca4ab2d84b2214ffa0fd
Mohamed AIT MANSOUR - UBUNTU <mohamedaitmansour95@gmail.com>
2018-05-14 15:27:43 +0200

    formatRtpStream : Format Rto Stream

--- src/StreamRTP.java
+++ src/StreamRTP.java
@@ -6,9 +6,25 @@
  */
 public class StreamRTP {
 
-	public static void main(String[] args) {
-		System.out.println("Fisrt Class");
+    public static void main(String[] args) {
+        System.out.println("Fisrt Class");
 
-	}
+    }
+
+    /**
+     * 
+     * @param serverAddress
+     * @param serverPort
+     * @return  mediaOptions for rtp stream wih vlc
+     */
+    private static String formatRtpStream(String serverAddress, int serverPort) {
+        StringBuilder sb = new StringBuilder(60);
+        sb.append(":sout=#rtp{dst=");
+        sb.append(serverAddress);
+        sb.append(",port=");
+        sb.append(serverPort);
+        sb.append(",mux=ts}");
+        return sb.toString();
+    }
 
 }
