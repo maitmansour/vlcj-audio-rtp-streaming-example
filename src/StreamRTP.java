25769caf6275fe95a959510d4927fe5e295b7ea0
Mohamed AIT MANSOUR - UBUNTU <mohamedaitmansour95@gmail.com>
2018-05-14 15:22:40 +0200

    INIT PROJECT

---
+++ src/StreamRTP.java
@@ -0,0 +1,14 @@
+/**
+ * 
+ * RTP Streaming using VLCJ
+ * @author Mohamed AIT MANSOUR
+ *
+ */
+public class StreamRTP {
+
+	public static void main(String[] args) {
+		System.out.println("Fisrt Class");
+
+	}
+
+}
