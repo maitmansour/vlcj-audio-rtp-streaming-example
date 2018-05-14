e6693c37c780ce42669db93bcc258508455de748
Mohamed AIT MANSOUR - UBUNTU <mohamedaitmansour95@gmail.com>
2018-05-14 15:47:41 +0200

    FINAL DEV COMMIT

--- src/MainApplication.java
+++ src/MainApplication.java
@@ -11,11 +11,13 @@ public class MainApplication {
 	    */
 	    public static void main(String[] args) {
 	    	try {
+	    		
 	    		System.out.println("Begin Streaming APP");
 				StreamRTP rtp = new StreamRTP();
 				//rtp.start("YOUR_LOCAL_IP_HERE", PORT, "sample.mp3");
 				rtp.start("10.20.11.142", 5000, "sample.mp3");
 	    		System.out.println("End Streaming APP");
+	    		
 			} catch (Exception e) {
 				e.printStackTrace();
 			}
